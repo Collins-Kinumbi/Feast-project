31f29ef127188797a3151246684e56853dc5098d
Collins-Kinumbi <kinumbicollins254@gmail.com>
2024-10-07 17:13:42 +0300

    Connecting to database logic moved to server.js

--- Server/app.js
+++ Server/app.js
@@ -1,33 +1,10 @@
 import express from "express";
 import morgan from "morgan";
-import mongoose from "mongoose";
 
 const app = express();
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-
-// Connect to database
-// mongoose
-//   .connect(process.env.CONNECTION_STRING)
-//   .then((req) => {
-//     console.log("Connection to database successfull...");
-//   })
-//   .catch((err) => {
-//     console.log(err.message);
-//   });
-
-async function connect() {
-  await mongoose.connect(process.env.CONNECTION_STRING);
-  console.log("Connection to database successfull...");
-}
-connect().catch((err) => {
-  console.log(err.message);
-});
-
-app.get("/", (req, res) => {
-  res.send("hello");
-});
 
 export default app;

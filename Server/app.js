28543a1cf71d87a2b95b3ca8b3f9855df4163a01
Collins-Kinumbi <kinumbicollins254@gmail.com>
2024-10-07 17:39:48 +0300

    Making HTTP requests

--- Server/app.js
+++ Server/app.js
@@ -6,5 +6,40 @@ const app = express();
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+app.use(express.json());
+
+/* API recipe routes */
+// All recipes
+app
+  .route("/api/v1/recipes")
+  // GET all recipes
+  .get((req, res) => {
+    res.status(200).json({ message: "All gotten!" });
+  })
+  // POST/ADD a recipe
+  .post((req, res) => {
+    console.log(req.body);
+    res.status(200).json({ message: "Posted!" });
+  });
+
+//Single recipe
+app
+  .route("/api/v1/recipes/:id")
+  // GET a recipe
+  .get((req, res) => {
+    console.log(req.params);
+    res.status(200).json({ message: "Gotten by id!" });
+  })
+  // UPDATE a recipe
+  .patch((req, res) => {
+    console.log(req.params);
+    console.log(req.body);
+    res.status(200).json({ message: "Patch!" });
+  })
+  // Delete a recipe
+  .delete((req, res) => {
+    console.log(req.params);
+    res.status(200).json({ message: "Deleted!" });
+  });
 
 export default app;

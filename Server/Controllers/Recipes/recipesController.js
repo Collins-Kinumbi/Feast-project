7dd140e582e288643ef64ef81ff81ebe18332950
Collins-Kinumbi <kinumbicollins254@gmail.com>
2024-10-07 19:09:12 +0300

    POST a recipe

--- Server/Controllers/Recipes/recipesController.js
+++ Server/Controllers/Recipes/recipesController.js
@@ -1,3 +1,5 @@
+import Recipe from "../../Models/Recipe/recipeModel.js";
+
 // Route handlers
 export function getRecipes(req, res) {
   res.status(200).json({ message: "All gotten!" });
@@ -8,9 +10,22 @@ export function getRecipe(req, res) {
   res.status(200).json({ message: "Gotten by id!" });
 }
 
-export function addRecipe(req, res) {
+export async function addRecipe(req, res) {
   console.log(req.body);
-  res.status(200).json({ message: "Posted!" });
+  try {
+    const recipe = await Recipe.create(req.body);
+    res.status(200).json({
+      status: "Success!",
+      data: {
+        recipe,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "Failed!",
+      message: err.message,
+    });
+  }
 }
 
 export function updateRecipe(req, res) {

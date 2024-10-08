4c7ff5d57553858c1ed671e82bf3a99f31190bfc
Collins-Kinumbi <kinumbicollins254@gmail.com>
2024-10-08 20:31:44 +0300

    New recipe schema

--- Server/Models/Recipe/recipeModel.js
+++ Server/Models/Recipe/recipeModel.js
@@ -1,21 +1,76 @@
 import mongoose from "mongoose";
 
 // Recipe schema
-const recipeSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "Title is required!"],
+const recipeSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Title field is required!"],
+      unique: true,
+    },
+    image: {
+      type: String,
+      required: [true, "Image field is required!"],
+    },
+    description: {
+      type: String,
+      required: [true, "Description field is required!"],
+    },
+    nutrition: [
+      {
+        calories: {
+          type: Number,
+          required: [true, "Calories field is required!"],
+        },
+        fats: {
+          type: Number,
+          required: [true, "Fats field is required!"],
+        },
+        carbohydrates: {
+          type: Number,
+          required: [true, "Carbohydrates field is required!"],
+        },
+        fibers: {
+          type: Number,
+          required: [true, "Fibers field is required!"],
+        },
+        sodium: {
+          type: Number,
+          required: [true, "Sodium field is required!"],
+        },
+        vitamins: {
+          type: Number,
+          required: [true, "Vitamins field is required!"],
+        },
+        minerals: {
+          type: Number,
+          required: [true, "Minerals field is required!"],
+        },
+      },
+    ],
+    ingredients: {
+      type: [String],
+      required: [true, "Ingredients field is required!"],
+    },
+    instructions: {
+      type: String,
+      required: [true, "Instructions field is required!"],
+    },
+    tags: {
+      type: [String],
+      required: [true, "Tags field is required!"],
+    },
+    serving: {
+      type: Number,
+      required: [true, "Serving field is required!"],
+    },
+    yield: {
+      type: Number,
+      required: [true, "yield field is required!"],
+    },
   },
-  ingredients: {
-    type: [String],
-    required: [true, "Title is required!"],
-  },
-  instructions: {
-    type: String,
-    required: [true, "Title is required!"],
-  },
-  tags: [String],
-});
+  { timestamps: true }
+);
 
 // movie model
 const Recipe = mongoose.model("Recipe", recipeSchema);

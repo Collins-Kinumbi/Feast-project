3a7dfeef4e5657ff5be344e5ebb6fbab449c0341
Collins-Kinumbi <kinumbicollins254@gmail.com>
2024-10-15 14:49:57 +0300

    Getting started on development and production errors

--- Server/Controllers/Errors/errorController.js
+++ Server/Controllers/Errors/errorController.js
@@ -1,14 +1,39 @@
+function developmentErrors(res, error) {
+  res.status(error.statusCode).json({
+    statusCode: error.statusCode,
+    message: error.message,
+    stackTrace: error.stack,
+    error: error,
+  });
+}
+
+function productionErrors(res, error) {
+  if (error.isOperational) {
+    res.status(error.statusCode).json({
+      statusCode: error.statusCode,
+      status: error.status,
+      message: error.message,
+    });
+  } else {
+    res.status(500).json({
+      statusCode: "Internal server error!",
+      message: "Something went wrong please try again later...",
+    });
+  }
+}
+
+//////////////////////////////////////////////////
 function globalErrorHandler(error, req, res, next) {
   // console.log("Error");
   error.statusCode = error.statusCode || 500;
   error.status = error.status || "Error";
 
   // Sending error response
-  res.status(error.statusCode).json({
-    statusCode: error.statusCode,
-    status: error.status,
-    message: error.message,
-  });
+  if (process.env.NODE_ENV === "development") {
+    developmentErrors(res, error);
+  } else if (process.env.NODE_ENV === "production") {
+    productionErrors(res, error);
+  }
 
   next();
 }

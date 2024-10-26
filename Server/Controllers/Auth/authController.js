3ceb1ecd9668c68a5ac44f56f8f2f9e87f82271b
Collins-Kinumbi <kinumbicollins254@gmail.com>
2024-10-26 16:33:35 +0300

    Signing up a user account and sending a jwt token

--- Server/Controllers/Auth/authController.js
+++ Server/Controllers/Auth/authController.js
@@ -1,12 +1,26 @@
 import User from "../../Models/User/userModel.js";
 import asyncErrorHandler from "../../Utils/asyncErrorHandler.js";
+import jwt from "jsonwebtoken";
 
+// Create account
 export const signup = asyncErrorHandler(async function (req, res, next) {
   // 1. Create the new user
-  const user = await User.create(req.body);
+  const { username, email, password, confirmPassword } = req.body;
+
+  const user = await User.create(username, email, password, confirmPassword);
+
+  // 2. Create a json web token
+  const token = jwt.sign(
+    { id: user._id, user: user.email },
+    process.env.SECRET_STRING || "Secret string",
+    {
+      expiresIn: process.env.LOGIN_EXPIRES || "1d",
+    }
+  );
 
   res.status(201).json({
     status: "Success!",
+    token, //Sending the token back to the client
     data: {
       user,
     },

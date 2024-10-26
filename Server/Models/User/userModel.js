d8f4cf9884ff4edcaf6cfe0a9afa20e80d7446d7
Collins-Kinumbi <kinumbicollins254@gmail.com>
2024-10-26 15:52:34 +0300

    Added timestamps to user schema

--- Server/Models/User/userModel.js
+++ Server/Models/User/userModel.js
@@ -3,40 +3,43 @@ import validator from "validator";
 import bcrypt from "bcryptjs";
 
 // User schema
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: [true, "Username is requred!"],
-    unique: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: [true, "Please enter an email!"],
-    unique: true,
-    lowercase: true,
-    trim: true,
-    validate: [validator.isEmail, "Please enter a valid email!"],
-  },
-  avatar: String,
-  password: {
-    type: String,
-    required: [true, "Please enter a password!"],
-    minlength: 8,
-    // select: false, // to prevent password from being returned in queries
-  },
-  confirmPassword: {
-    type: String,
-    required: [true, "Please confirm your password!"],
-    validate: {
-      // This only works on CREATE and SAVE (not on UPDATE)
-      validator: function (value) {
-        return value === this.password;
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is requred!"],
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter an email!"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate: [validator.isEmail, "Please enter a valid email!"],
+    },
+    avatar: String,
+    password: {
+      type: String,
+      required: [true, "Please enter a password!"],
+      minlength: 8,
+      // select: false, // to prevent password from being returned in queries
+    },
+    confirmPassword: {
+      type: String,
+      required: [true, "Please confirm your password!"],
+      validate: {
+        // This only works on CREATE and SAVE (not on UPDATE)
+        validator: function (value) {
+          return value === this.password;
+        },
+        message: "Passwords do not match!",
       },
-      message: "Passwords do not match!",
     },
   },
-});
+  { timestamps: true }
+);
 
 //Encrypting password before saving it to the database
 userSchema.pre("save", async function (next) {

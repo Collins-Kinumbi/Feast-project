9e86acce869da7dc2e5438804c035448e690feb0
Collins-Kinumbi <kinumbicollins254@gmail.com>
2024-11-09 14:21:34 +0300

    Date formatting

--- client/src/components/Recipe/Recipe.jsx
+++ client/src/components/Recipe/Recipe.jsx
@@ -1,10 +1,19 @@
 function Recipe({ recipe }) {
+  // Date formatting
+  const date = new Date(recipe.createdAt);
+
+  const uploadedOn = date.toLocaleDateString("en-GB", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
   return (
     <div className="recipe">
       <img src="images/placeholder/placeholder.png" alt={`${recipe.name}`} />
       <div className="content">
         <p className="uploadedOn">
-          Uploaded on: <span>{recipe.createdAt}</span>
+          Uploaded on: <span>{uploadedOn}</span>
         </p>
         <p className="recipeName">{recipe.name}</p>
         <p className="user">

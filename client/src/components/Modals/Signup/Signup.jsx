91c93919eaa970c4f4a8b03253bcb8039e1cad34
Collins-Kinumbi <kinumbicollins254@gmail.com>
2024-11-20 16:34:49 +0300

    Sign up form functionality

--- client/src/components/Modals/Signup/Signup.jsx
+++ client/src/components/Modals/Signup/Signup.jsx
@@ -1,4 +1,51 @@
+import { useState } from "react";
+
 function Signup({ onClose }) {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  async function signup(email, username, password, confirmPassword) {
+    setError(null);
+
+    const userData = {
+      email,
+      username,
+      password,
+      confirmPassword,
+    };
+    try {
+      const response = await fetch("http://localhost:4000/api/v1/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+        credentials: "include",
+      });
+      // console.log(response);
+
+      const resData = await response.json();
+      console.log(resData);
+
+      if (!response.ok) {
+        throw new Error(resData.message);
+      }
+      onClose();
+    } catch (error) {
+      // console.log(error);
+      setError(error.message);
+    }
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    // console.log(
+    //   `Email: ${email}, Username: ${username}, Password: ${password}, confirmPassword: ${confirmPassword}`
+    // );
+    signup(email, username, password, confirmPassword);
+  }
   return (
     <>
       <div className="modal">
@@ -9,13 +56,37 @@ function Signup({ onClose }) {
           </button>
           <div className="form-content">
             <h2>Sign up</h2>
-            <form action="">
-              <input type="email" required placeholder="Email" />
-              <input type="text" required placeholder="Username" />
+            <form onSubmit={handleSubmit}>
+              <input
+                type="email"
+                required
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="text"
+                required
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
 
-              <input type="password" required placeholder="Password" />
-              <input type="password" required placeholder="Confirm password" />
-
+              <input
+                type="password"
+                required
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <input
+                type="password"
+                required
+                placeholder="Confirm password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+              {error && <p className="error">{error}</p>}
               <button type="submit" className="sign-up-btn">
                 Sign up
               </button>

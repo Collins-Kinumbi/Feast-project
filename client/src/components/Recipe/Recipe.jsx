4d9741bd4ba811e510730350c7d7809a45a00e25
Collins Kinumbi <kinumbicollins254@gmail.com>
2024-12-11 15:38:53 -0800

    Fetching users by id

--- client/src/components/Recipe/Recipe.jsx
+++ client/src/components/Recipe/Recipe.jsx
@@ -1,7 +1,33 @@
 import { Link } from "react-router-dom";
 import formatDate from "../../utils/Date";
+import { useEffect, useState } from "react";
 
 function Recipe({ recipe }) {
+  const [username, setUsername]= useState('')
+  const[error,setError]= useState(null)
+useEffect(()=>{
+  async function getUser(id) {
+    setError(null)
+    try {
+      const response = await fetch(`http://localhost:4000/api/v1/users/${id}`);
+
+      // console.log(response);
+      if (!response.ok) {
+        throw new Error("Sorry something went wrong...");
+      }
+      const resData = await response.json();
+      console.log(resData)
+      const username = resData.data.user.username
+      setUsername(username)
+      
+
+    } catch (error) {
+      setError(error.message)
+    }
+  }
+getUser(recipe.user)
+},[recipe.user])
+
   return (
     <Link to={`/recipe/${recipe._id}`}>
       <div className="recipe">
@@ -12,7 +38,7 @@ function Recipe({ recipe }) {
           </p>
           <p className="recipeName">{recipe.name}</p>
           <p className="user">
-            By <span>{recipe.user}</span>
+          By <span>{error ? "Unknown" : username || "Loading..."}</span>
           </p>
         </div>
       </div>

0450e77748a82bf47241a8431d10572a52d616ac
Collins Kinumbi <kinumbicollins254@gmail.com>
2024-12-18 10:51:41 -0800

    Recipe component code reformat

--- client/src/components/Recipe/Recipe.jsx
+++ client/src/components/Recipe/Recipe.jsx
@@ -5,39 +5,41 @@ import { useEffect, useState } from "react";
 const userCache = new Map(); // Local cache for user data
 
 function Recipe({ recipe }) {
-  const [username, setUsername]= useState('')
-  const[error,setError]= useState(null)
-useEffect(()=>{
-  async function getUser(id) {
-    setError(null)
+  const [username, setUsername] = useState("");
+  const [error, setError] = useState(null);
 
-    // Check cache first
-    if (userCache.has(id)) {
-      setUsername(userCache.get(id));
-      return;
-    }
+  useEffect(() => {
+    async function getUser(id) {
+      setError(null);
 
-    try {
-      const response = await fetch(`http://localhost:4000/api/v1/users/${id}`);
-
-      // console.log(response);
-      if (!response.ok) {
-        throw new Error("Sorry something went wrong...");
+      // Check cache first
+      if (userCache.has(id)) {
+        setUsername(userCache.get(id));
+        return;
       }
-      const resData = await response.json();
-      
-      const username = resData.data.user.username
-      
-      // Update state and cache
-      setUsername(username)
-      userCache.set(id, username);
 
-    } catch (error) {
-      setError(error.message)
+      try {
+        const response = await fetch(
+          `http://localhost:4000/api/v1/users/${id}`
+        );
+
+        // console.log(response);
+        if (!response.ok) {
+          throw new Error("Sorry something went wrong...");
+        }
+        const resData = await response.json();
+
+        const username = resData.data.user.username;
+
+        // Update state and cache
+        setUsername(username);
+        userCache.set(id, username);
+      } catch (error) {
+        setError(error.message);
+      }
     }
-  }
-getUser(recipe.user)
-},[recipe.user])
+    getUser(recipe.user);
+  }, [recipe.user]);
 
   return (
     <Link to={`/recipe/${recipe._id}`}>
@@ -49,7 +51,7 @@ getUser(recipe.user)
           </p>
           <p className="recipeName">{recipe.name}</p>
           <p className="user">
-          By <span>{error ? "Unknown" : username || "Loading..."}</span>
+            By <span>{error ? "Unknown" : username || "Loading..."}</span>
           </p>
         </div>
       </div>

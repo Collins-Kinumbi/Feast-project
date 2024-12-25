ced40989891dfc42c0b1e20a20ad005e28007f97
Collins Kinumbi <kinumbicollins254@gmail.com>
2024-12-25 12:43:10 -0800

    Pagination in categories page

--- client/src/pages/Categories/Categories.jsx
+++ client/src/pages/Categories/Categories.jsx
@@ -1,12 +1,17 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import Recipe from "../../components/Recipe/Recipe";
+import Pagination from "../../components/Pagination/Pagination";
 
 function Categories() {
   const { category } = useParams(); // Get category from URL params
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentPage = Number(searchParams.get("page")) || 1;
+
   const [recipes, setRecipes] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [totalPages, setTotalPages] = useState(0);
 
   useEffect(() => {
     async function fetchCategoryRecipes() {
@@ -14,13 +19,14 @@ function Categories() {
       try {
         setError(null);
         const response = await fetch(
-          `http://localhost:4000/api/v1/recipes?category=${category}`
+          `http://localhost:4000/api/v1/recipes?category=${category}&page=${currentPage}`
         );
         if (!response.ok) {
           throw new Error("Failed to fetch recipes for the selected category.");
         }
         const resData = await response.json();
-        setRecipes(resData.data.recipes); // Adjust based on API structure
+        setRecipes(resData.data.recipes);
+        setTotalPages(resData.totalPages);
       } catch (err) {
         setError(err.message);
       } finally {
@@ -29,7 +35,12 @@ function Categories() {
     }
 
     fetchCategoryRecipes();
-  }, [category]);
+  }, [category, currentPage]);
+
+  // Handle page change
+  function handlePageChange(newPage) {
+    setSearchParams({ page: newPage }); // Update page in URL params
+  }
 
   return (
     <div className="categories-page">
@@ -38,7 +49,16 @@ function Categories() {
       {isLoading ? (
         <p>Loading...</p>
       ) : recipes.length > 0 ? (
-        <Recipe recipes={recipes} loading={isLoading} />
+        <>
+          <Recipe recipes={recipes} loading={isLoading} />
+          <div className="pagination-container">
+            <Pagination
+              currentPage={currentPage}
+              totalPages={totalPages}
+              onPageChange={handlePageChange}
+            />
+          </div>
+        </>
       ) : (
         <p>OOOPS guess there are no {category} recipes yet.</p>
       )}

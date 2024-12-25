42c0ecb8b39b4eefdf36a76c94a54b92ee3efdf2
Collins Kinumbi <kinumbicollins254@gmail.com>
2024-12-24 17:16:19 -0800

    <div> cannot appear as a descendant of <p>. error fix

--- client/src/components/Lazy Load Image/LazyLoadedImage.jsx
+++ client/src/components/Lazy Load Image/LazyLoadedImage.jsx
@@ -3,15 +3,13 @@ import { useState } from "react";
 function LazyLoadedImage({ src, alt, className }) {
   const [isLoaded, setIsLoaded] = useState(false);
   return (
-    <div className="image-wrapper" style={{ position: "relative" }}>
-      <img
-        src={src || "images/placeholder/placeholder.jpg"}
-        alt={alt}
-        loading="lazy"
-        className={`lazy-image ${isLoaded ? "loaded" : ""} ${className}`}
-        onLoad={() => setIsLoaded(true)}
-      />
-    </div>
+    <img
+      src={src || "images/placeholder/placeholder.jpg"}
+      alt={alt}
+      loading="lazy"
+      className={`lazy-image ${isLoaded ? "loaded" : ""} ${className}`}
+      onLoad={() => setIsLoaded(true)}
+    />
   );
 }
 

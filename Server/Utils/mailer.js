47a56a34e7e00131f710d326f6e4f8aa058f2b02
Collins Kinumbi <kinumbicollins254@gmail.com>
2024-12-30 15:09:46 -0800

    Sending email to user  gmail

--- Server/Utils/mailer.js
+++ Server/Utils/mailer.js
@@ -1,24 +1,48 @@
 import nodemailer from "nodemailer";
 
 async function sendEmail(option) {
-  // Create a transpoter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: false, // true for port 465, false for other ports
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+  let transporter;
+
+  if (process.env.NODE_ENV === "development") {
+    // Use Mailtrap for development
+    transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      secure: false, // true for port 465, false for other ports
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  } else {
+    // Use Gmail for production
+    transporter = nodemailer.createTransport({
+      service: "gmail",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.GMAIL_USERNAME,
+        pass: process.env.GMAIL_PASSWORD,
+      },
+    });
+  }
+
   // Define email options
   const emailOptions = {
-    from: "Feast support<support@feast.com>",
+    from: "Feast Support <support@feast.com>",
     to: option.email,
     subject: option.subject,
     text: option.message,
   };
-  await transporter.sendMail(emailOptions); //Send the email
+
+  // Send the email
+  try {
+    await transporter.sendMail(emailOptions);
+    console.log("Email sent successfully.");
+  } catch (error) {
+    console.error("Failed to send email:", error);
+    throw error;
+  }
 }
 
 export default sendEmail;

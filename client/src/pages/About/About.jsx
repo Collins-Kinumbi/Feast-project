1421be7a85c8401a2e4ae80cfd6d2f9a92cd6c83
Collins Kinumbi <kinumbicollins254@gmail.com>
2024-12-31 13:03:30 -0800

    About page

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -1,7 +1,33 @@
 function About() {
   return (
-    <div>
-      <h2>About page</h2>
+    <div className="about-page">
+      <p>
+        Hey, welcome to Feast. This is a web app for culinary enthusiasts. Here
+        you can share your recipes with others and you can also access recipes
+        created by other users, you can edit your own recipes and also delete
+        them if you wish....This is a <span>personal project</span> for my{" "}
+        <a
+          href="https://kinumbi.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+          title="Visit my portfolio"
+        >
+          portfolio
+        </a>
+        . I have tonnes of features in mind to add to this app in the future as
+        I broaden my understanding in the wizardry arts of coding. I hope you
+        like it and if you have any suggestions on features I should add or if
+        you'd like to contact me in general you can hit me up on{" "}
+        <a
+          href="https://x.com/collinsKinumbi"
+          target="_blank"
+          rel="noreferrer"
+          title="Connect with me on Twitter"
+        >
+          twitter
+        </a>
+        . Any way have fun, create some recipes and share them with the world.
+      </p>
     </div>
   );
 }
